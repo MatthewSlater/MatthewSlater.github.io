6973e4b1ce85e50ec67ac762cec0b7f25bb94996
MatthewSlater <matthew@le-stars.com>
2016-12-23 18:03:10 +0000

    Gained missing css and changed service worker to register sw.js

--- js/serviceWorker.js
+++ js/serviceWorker.js
@@ -1,6 +1,6 @@
 ﻿if ('serviceWorker' in navigator) {
     window.addEventListener('load', function () {
-        navigator.serviceWorker.register('/js/serviceWorker.js').then(function (registration) {
+        navigator.serviceWorker.register('/js/sw.js').then(function (registration) {
             // Registration was successful
             console.log('ServiceWorker registration successful with scope: ', registration.scope);
         }).catch(function (err) {
