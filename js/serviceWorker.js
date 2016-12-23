b39fa11b8ee11097de376549fef7fdfe5e6af1a0
MatthewSlater <matthew@le-stars.com>
2016-12-23 17:55:21 +0000

    Added offline service worker

---
+++ js/serviceWorker.js
@@ -0,0 +1,37 @@
+﻿if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function () {
+        navigator.serviceWorker.register('/js/serviceWorker.js').then(function (registration) {
+            // Registration was successful
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        }).catch(function (err) {
+            // registration failed :(
+            console.log('ServiceWorker registration failed: ', err);
+        });
+    });
+}
+
+self.addEventListener('install', function (event) {
+    event.waitUntil(
+      caches.open('acw-v1').then(function (cache) {
+          return cache.addAll([
+            '/index.html',
+            '/js/matrix.js',
+            '/js/vector.js',
+            '/js/sceneNode.js',
+            '/js/house.js',
+            '/js/door.js',
+            '/js/roof.js',
+            '/js/wall.js',
+            '/js/window.js'
+          ]);
+      })
+    );
+});
+
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+      fetch(event.request).catch(function () {
+          return caches.match(event.request);
+      })
+    );
+});
