90c62795e041e01d7e18be426a3cca3a16e2536b
MatthewSlater <matthew@le-stars.com>
2016-12-23 18:18:22 +0000

    Changed the location of sw

--- js/serviceWorker.js
+++ js/serviceWorker.js
@@ -1,6 +1,6 @@
 ﻿if ('serviceWorker' in navigator) {
     window.addEventListener('load', function () {
-        navigator.serviceWorker.register('/js/sw.js').then(function (registration) {
+        navigator.serviceWorker.register('sw.js').then(function (registration) {
             // Registration was successful
             console.log('ServiceWorker registration successful with scope: ', registration.scope);
         }).catch(function (err) {
@@ -9,29 +9,3 @@
         });
     });
 }
-
-self.addEventListener('install', function (event) {
-    event.waitUntil(
-      caches.open('acw-v1').then(function (cache) {
-          return cache.addAll([
-            '/index.html',
-            '/js/matrix.js',
-            '/js/vector.js',
-            '/js/sceneNode.js',
-            '/js/house.js',
-            '/js/door.js',
-            '/js/roof.js',
-            '/js/wall.js',
-            '/js/window.js'
-          ]);
-      })
-    );
-});
-
-self.addEventListener('fetch', function (event) {
-    event.respondWith(
-      fetch(event.request).catch(function () {
-          return caches.match(event.request);
-      })
-    );
-});
---
+++ sw.js
@@ -0,0 +1,25 @@
+﻿self.addEventListener('install', function (event) {
+    event.waitUntil(
+      caches.open('acw-v1').then(function (cache) {
+          return cache.addAll([
+            '/index.html',
+            '/js/matrix.js',
+            '/js/vector.js',
+            '/js/sceneNode.js',
+            '/js/house.js',
+            '/js/door.js',
+            '/js/roof.js',
+            '/js/wall.js',
+            '/js/window.js'
+          ]);
+      })
+    );
+});
+
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+      fetch(event.request).catch(function () {
+          return caches.match(event.request);
+      })
+    );
+});
